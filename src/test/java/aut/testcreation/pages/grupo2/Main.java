5f3c30a10a427230a281e6934902d82df89cbe59
Guido Papa Rúa <guido.paparua@tsoftglobal.com>
2023-10-06 11:08:53 -0300

    Se cargan los metodos al archivo base

--- src/test/java/aut/testcreation/pages/grupo2/Main.java
+++ src/test/java/aut/testcreation/pages/grupo2/Main.java
@@ -1,6 +1,10 @@
 package aut.testcreation.pages.grupo2;
 
 import framework.engine.utils.grupo2.Rumbo_Base;
+import org.openqa.selenium.WebDriver;
 
 public class Main extends Rumbo_Base {
+    public Main(WebDriver driver) {
+        super(driver);
+    }
 }
