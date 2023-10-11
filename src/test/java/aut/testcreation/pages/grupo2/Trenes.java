3e78802dbce6df77168f21df9087bf7c329be835
Guido Papa Rúa <guido.paparua@tsoftglobal.com>
2023-10-11 09:58:42 -0300

    subo prueba n5 de trenes

--- src/test/java/aut/testcreation/pages/grupo2/Trenes.java
+++ src/test/java/aut/testcreation/pages/grupo2/Trenes.java
@@ -15,6 +15,10 @@ public class Trenes extends Rumbo_Base {
     By locatorBtnDiaIda = By.xpath("//form/div[2]/div/div/div/div/div/section/div/div/div/div[2]/div[2]/button[6]") ;
     By locatorBtnDiaVuelta = By.xpath("//form/div[2]/div[2]/div/div/div/div/section/div/div/div/div[3]/div[2]/button[30]") ;
     By locatorBtnBuscar = By.xpath("//button[@type='submit']") ;
+    By locatorSoloIda = By.xpath("//div[contains(text(), 'Solo ida')]") ;
+    By locatorPrimerTren = By.xpath("//div[@class='trip-collection-view__trips-container-top']");
+    By locatorSeleccionar = By.xpath("//button[contains(text(), 'Seleccionar')]");
+    By locatorSinProteccionAdicional = By.xpath("//div[@class='insurance__noThanks-expandable-left-box']");
 
     public Trenes(WebDriver driver) {
         super(driver);
@@ -35,4 +39,30 @@ public class Trenes extends Rumbo_Base {
         click(esperarPorElementoLocalizado(locatorBtnDiaVuelta));
         click(esperarPorElementoLocalizado(locatorBtnBuscar));
     }
+    public void reservaSoloIda (String origen, String destino) {
+        click(esperarPorElementoLocalizado(locatorSoloIda));
+        click(esperarPorElementoLocalizado(locatorBtnOrigenTrenes));
+        escribirTexto(esperarPorElementoLocalizado(locatorBtnOrigenTrenes), origen);
+        esperarXSegundos(5000);
+        click(esperarPorElementoLocalizado(locatorMadrid));
+        esperarXSegundos(2000);
+        click(esperarPorElementoLocalizado(locatorBtnDestinoTrenes));
+        escribirTexto(esperarPorElementoLocalizado(locatorBtnDestinoTrenes), destino);
+        esperarXSegundos(5000);
+        click(esperarPorElementoLocalizado(locatorBarcelona));
+        click(esperarPorElementoLocalizado(locatorBtnFechaIda));
+        click(esperarPorElementoLocalizado(locatorBtnDiaIda));
+        click(esperarPorElementoLocalizado(locatorBtnBuscar));
+    }
+
+    public void seleccionarPrimerVuelo() {
+        click(esperarPorElementoLocalizado(locatorPrimerTren));
+        click(esperarPorElementoLocalizado(locatorSeleccionar));
+    }
+
+    public void sinProteccionAdicional () {
+        click(esperarPorElementoLocalizado(locatorSinProteccionAdicional));
+    }
+
+
 }
